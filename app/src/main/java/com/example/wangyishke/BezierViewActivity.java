dfbb104afff16dab2a30e071698f8bbc94fa448e
DavyJohn <1139099003@qq.com>
2016-11-21 13:24:37 +0800

    Initial commit

---
+++ app/src/main/java/com/example/wangyishke/BezierViewActivity.java
@@ -0,0 +1,18 @@
+package com.example.wangyishke;
+
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+
+/**
+ * Created by 腾翔信息 on 2016/11/21.
+ */
+
+public class BezierViewActivity  extends AppCompatActivity {
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_bezier_view);
+
+    }
+
+}
